fb11df888d741f4b0a12de461121431d6f0b4172
jjwon915 <jjwon915@naver.com>
2021-03-22 15:03:43 +0900

    - DataBase의 Column에 맞춰서 각 변수를 CamelCase방식으로 선언. (JPA의 Entity는 자동으로 snake_case로 매칭해줌.)

---
+++ src/main/java/com/example/study/model/entity/User.java
@@ -0,0 +1,34 @@
+package com.example.study.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+// DB의 Table 이름과 동일하게 생성한다면 @Table(name="user")안써도 됨.
+@Data
+@AllArgsConstructor
+@Entity // == table
+//@Table(name="user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // @Column(name="account") : 변수명과 Column명이 동일하면 작성하지 않아도 된다.
+    private Long id;
+
+    private String acoount;
+
+    private String email;
+
+    private String phoneNumber;
+
+    private LocalDateTime createdAt;
+
+    private String createdBy;
+
+    private LocalDateTime updatedAt;
+
+    private String updatedBy;
+}
