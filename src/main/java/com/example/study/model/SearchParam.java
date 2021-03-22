564bffd6f989884c041e1e5e9f080c5ce64385a5
jjwon915 <jjwon915@naver.com>
2021-03-22 12:12:59 +0900

    Lombok Plugin install - Lombok을 사용하면 java code의 길이를 줄여줄 수 있고 생산성을 높일 수 있다.

--- src/main/java/com/example/study/model/SearchParam.java
+++ src/main/java/com/example/study/model/SearchParam.java
@@ -1,34 +1,16 @@
 package com.example.study.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor // 모든 매개변수를 가지는 생성자가 추가된다. -> java code를 줄여줌으로써 생산성을 높여줌.
 public class SearchParam {
     //getMultiParameter에 사용할 객체.
     private String account;
     private String email;
     private int page;
 
-   //{"account" : "", "email" : "", "page": 0}
+    //{"account" : "", "email" : "", "page": 0}
 
-    public String getAccount() {
-        return account;
-    }
-
-    public void setAccount(String account) {
-        this.account = account;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getPage() {
-        return page;
-    }
-
-    public void setPage(int page) {
-        this.page = page;
-    }
 }
